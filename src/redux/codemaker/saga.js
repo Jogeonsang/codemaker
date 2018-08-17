12790e55475e0da9e66b30d2dcb1893d3cebfcc1
Jogeonsang <rhcn258@gmail.com>
2018-08-17 16:32:41 +0900

    redux-saga

---
+++ src/redux/codemaker/saga.js
@@ -0,0 +1,10 @@
+import { takeEvery, put, call } from 'redux-saga/effects';
+
+import * as service from '../../api/codemaker';
+import {
+    SUBMIT_DATA, submitData
+} from './action';
+
+export function* submitData(action) {
+    const { }
+}
